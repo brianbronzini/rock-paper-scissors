c9e59895dc378a83c3547febec6d3fffb0ee38d5
brianveebee <brianbronzini@gmail.com>
2022-08-02 18:22:00 -0700

    Add win/loss conditions

--- js/rps.js
+++ js/rps.js
@@ -6,3 +6,26 @@ let getComputerChoice = () => {
   
   return choice;
 }
+
+// Set all possible win/loss/tie conditions and increment the counters
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log('Tie! Go again!');
+  }
+  for (let playerScore = 0; playerScore < 3; playerScore++) {
+    if (
+      (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+      (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+      (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+    )
+    console.log(`You win this round! ${playerSelection} beats ${computerSelection}`);
+  }
+  for (let computerScore = 0; computerScore < 3; computerScore++) {
+    if (
+      (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+      (computerSelection === "PAPER" && playerSelection === "ROCK") ||
+      (computerSelection === "SCISSORS" && playerSelection === "PAPER")
+    )
+    console.log(`Computer wins this round.. ${computerSelection} beats ${playerSelection}`);
+  }
+}
