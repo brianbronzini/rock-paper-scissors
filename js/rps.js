3d1b5864627bfa5fd98fefd4949a164a91d00a5c
brianveebee <brianbronzini@gmail.com>
2022-08-02 18:37:29 -0700

    Loop through 5 rounds and get selections

--- js/rps.js
+++ js/rps.js
@@ -7,25 +7,37 @@ let getComputerChoice = () => {
   return choice;
 }
 
-// Set all possible win/loss/tie conditions and increment the counters
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    console.log('Tie! Go again!');
+// Loop through 5 rounds and get choices from the computer/player
+function game() {
+  for (let round = 1; round <= 5; round++) {
+    const playerSelection = prompt('Please choose (Rock, Paper, or Scissors): ').toUpperCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
   }
-  for (let playerScore = 0; playerScore < 3; playerScore++) {
-    if (
-      (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
-      (playerSelection === "PAPER" && computerSelection === "ROCK") ||
-      (playerSelection === "SCISSORS" && computerSelection === "PAPER")
-    )
-    console.log(`You win this round! ${playerSelection} beats ${computerSelection}`);
-  }
-  for (let computerScore = 0; computerScore < 3; computerScore++) {
-    if (
-      (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
-      (computerSelection === "PAPER" && playerSelection === "ROCK") ||
-      (computerSelection === "SCISSORS" && playerSelection === "PAPER")
-    )
-    console.log(`Computer wins this round.. ${computerSelection} beats ${playerSelection}`);
+
+  // Set all possible win/loss/tie conditions and increment the counters
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      console.log('Tie! Go again!');
+    }
+    for (let playerScore = 0; playerScore < 3; playerScore++) {
+      if (
+        (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+        (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+      )
+      console.log(`You win this round! ${playerSelection} beats ${computerSelection}`);
+    }
+    for (let computerScore = 0; computerScore < 3; computerScore++) {
+      if (
+        (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+        (computerSelection === "PAPER" && playerSelection === "ROCK") ||
+        (computerSelection === "SCISSORS" && playerSelection === "PAPER")
+      )
+      console.log(`Computer wins this round.. ${computerSelection} beats ${playerSelection}`);
+    }
   }
 }
+
+// Run the game
+game();
