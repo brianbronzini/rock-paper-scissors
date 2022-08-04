85ca9ac088c1666fa3a8268675acb76bad127fdd
brianveebee <brianbronzini@gmail.com>
2022-08-03 17:32:58 -0700

    Check for valid user input and add gameover alert

--- js/rps.js
+++ js/rps.js
@@ -9,17 +9,25 @@ function getComputerChoice() {
 
 // Loop through 5 rounds and get choices from the computer/player
 function game() {
-  // Initialize score counters for function scope
-  let playerScore, computerScore;
-  let round = 1;
+  // Initialize counters for function scope
+  let playerScore = 0, computerScore = 0, round = 1;
 
   while (round <= 5) {
     const playerSelection = prompt('Please choose (Rock, Paper, or Scissors): ').toUpperCase();
     const computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
     round++;
+    if (playerScore === 3) {
+      alert('YOU WIN! :D');
+      break;
+    }
+    else if (computerScore === 3) {
+      alert('YOU LOSE! D:');
+      break;
+    }
   }
 
+  // Handles tie condition
   function tie() {
     round--;
     console.log('Tie! Go Again!');
@@ -30,13 +38,17 @@ function game() {
     if (playerSelection === computerSelection) {
       tie();
     }
+    else if (playerSelection !== 'ROCK' && playerSelection !== 'PAPER' && playerSelection !== 'SCISSORS') {
+      round--;
+      alert('Please enter a valid response');
+    }
     else if (
       (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
       (playerSelection === "PAPER" && computerSelection === "ROCK") ||
       (playerSelection === "SCISSORS" && computerSelection === "PAPER")
     ) {
-      playerScore++;
-      console.log(`You win this round! ${playerSelection} beats ${computerSelection}`);
+        playerScore++;
+        console.log(`You win this round! ${playerSelection} beats ${computerSelection}`);
     }
     else {
       computerScore++;
